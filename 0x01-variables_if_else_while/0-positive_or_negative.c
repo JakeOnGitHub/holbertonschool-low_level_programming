af3373101b6c3ba8d3a7a73fb0b1dfbb2371137e
jakeongithub <jakecrandall@gmail.com>
2022-01-25 07:57:02 -0800

    negative or positive if statements commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+* main - entry point
+*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if [n > 0]
+do
+	printf("$n is positive");
+if [n = 0]
+do
+	printf("$n is zero");
+if [n < 0]
+do
+	printf("$n is negative");
+
+	((n++))
+fi
+fi
+fi
+	return (0);
+done
+done
+done
+}
