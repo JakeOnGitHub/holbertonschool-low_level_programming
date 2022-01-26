2a7149c7af2c08e28fb830376f29124a4041a09e
jakeongithub <jakecrandall@gmail.com>
2022-01-26 13:10:48 -0800

    commit main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,9 +3,11 @@
 /* includes */
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 /* prototypes */
 /* _putchar prototype */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 #endif /* MAIN_H */
