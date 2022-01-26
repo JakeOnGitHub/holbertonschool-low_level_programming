b1a8771027505d016827cd8534915fdced6d7056
jakeongithub <jakecrandall@gmail.com>
2022-01-26 09:31:07 -0800

    print alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - entry point
+*
+* Return: describes the value of the return
+*/
+int main(void)
+{
+for (int x = 'a'; x <= 'z'; x++)
+{
+	putchar(x);
+}
+{
+	putchar('\n');
+	return (0);
+}
+}
