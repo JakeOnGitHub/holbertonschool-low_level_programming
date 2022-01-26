5d983db314f51b9652f4eeabd57c9c720a507e95
jakeongithub <jakecrandall@gmail.com>
2022-01-26 12:53:10 -0800

    holy moly i'm confused commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*
 * _islower - checks for lowercase letters
-*
+* @c: letter to be checked ro upper or lower case
 * Return: returns 1 if lowercase.  ohterwise, returns 0
 */
 int _islower(int c)
 {
-	if (c == 'a' && c <= 'z')
+	if (c <= 'a' && c <= 'z')
 	{
-		putchar('1');
+		return('1');
 	}
-	else 
+	else
 	{
-		putchar('0');
+		return('0');
 	}
 	return (0);
 }
