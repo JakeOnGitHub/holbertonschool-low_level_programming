8f779310e3c3b2ef4b26f7cf4df163eb619da34b
jakeongithub <jakecrandall@gmail.com>
2022-01-26 11:56:48 -0800

    1 if lowercase - 0 if upper commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*
+* _islower - checks for lowercase letters
+*
+* Return: returns 1 if lowercase.  ohterwise, returns 0
+*/
+int _islower(int c)
+{
+char x;
+	if (x = 'a' && x <= 'z')
+	{
+		putchar('1');
+	}
+	else 
+	{
+		putchar('0');
+	}
+	return (0);
+}
