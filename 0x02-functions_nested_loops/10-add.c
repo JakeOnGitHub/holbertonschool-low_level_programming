039372bac6c844c1e7be237d982081bb6db8e348
jakeongithub <jakecrandall@gmail.com>
2022-01-26 19:34:34 -0800

    add two integers commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* add - fuction that adds numbers
+* @x: the value of 1 integer passed to the function
+* @y: the value of another integer passed to the function
+* Return: values passed to the console
+*/
+int add(int x, int y)
+{
+	int result;
+	{
+	result = x + y;
+	}
+	return (result);
+}
