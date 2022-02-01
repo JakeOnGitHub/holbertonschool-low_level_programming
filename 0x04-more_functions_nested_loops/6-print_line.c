49fda14e08332f3ba1f5ed46ad9ddd4eeeb883d9
jakeongithub <jakecrandall@gmail.com>
2022-02-01 15:44:18 -0800

    print lines commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_line - function to print the line
+* @n: number of spaces inputted
+* Return: void
+*/
+
+void print_line(int n)
+{
+	int line = 0;
+	
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+		{
+			_putchar(95);
+		}
+		{
+			_putchar('\n');
+		}
+	}
+
+}
