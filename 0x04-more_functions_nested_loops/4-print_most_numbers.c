d9af741afab1aebb80c29a4c7086f2a88b2eae32
jakeongithub <jakecrandall@gmail.com>
2022-02-01 12:08:22 -0800

    hella changes commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_most_numbers - function we're creating
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+		}
+		else
+		{
+			_putchar(x);
+			x++;
+		}
+	{
+		_putchar('\n');
+	}
+	}
+}
