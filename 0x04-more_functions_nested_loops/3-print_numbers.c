fec8d1369d3f04fe1994fa2557335951c83094a7
jakeongithub <jakecrandall@gmail.com>
2022-02-01 09:08:02 -0800

    print numbers commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+* print_numbers: function to print numbers
+* @i: integer we will will be printing
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0, i < 10, i++)
+	{
+		_putchar(i);
+	}
+	{
+		_putchar('\n');
+	}
+}
