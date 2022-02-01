861eac85c045c6081cdae25756f0cf8199cd1a23
jakeongithub <jakecrandall@gmail.com>
2022-02-01 10:54:33 -0800

    fixed my brain commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,16 @@
-#include <main.h>
+#include "main.h"
 /**
 * mul - function that multiplies two integers
 * @a: integer
 * @b: integer
-* Return: (0)
+* Return: (product)
 */
 
 int mul(int a, int b)
 {
-	int a;
-	int b;
 	int product;
-
+	{
 	product = a * b;
-
-	return (0);
+	}
+	return (product);
 }
