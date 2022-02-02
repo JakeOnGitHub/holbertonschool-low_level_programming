8dff84800101b41b6dfd7fb81e7ad562fa35851f
jakeongithub <jakecrandall@gmail.com>
2022-02-01 17:00:23 -0800

    print ssquares commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_square - function that prints square
+* @size: number of # in the square
+* Return: Void
+*/
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
