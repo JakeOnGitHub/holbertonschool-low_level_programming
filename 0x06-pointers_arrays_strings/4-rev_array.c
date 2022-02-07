ab9792239828956b86d1ab7d497edbf36ff45f2f
jakeongithub <jakecrandall@gmail.com>
2022-02-07 13:14:17 -0800

    print array of integers in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,7 @@ void reverse_array(int *a, int n)
 	int string;
 	int reverse;
 
-	for (string = n - 1; string >= 2; string--)
+	for (string = n - 1; string >= n / 2; string--)
 	{
 		reverse = a[string];
 		a[string] = a[n - 1 - string];
