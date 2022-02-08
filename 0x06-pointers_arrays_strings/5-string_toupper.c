77f8436d923fa09909537399dcda1f040a2022c1
jakeongithub <jakecrandall@gmail.com>
2022-02-07 16:24:03 -0800

    betty commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,9 +2,8 @@
 
 /**
 * string_toupper - function to convert strings to upper case
-*
-*
-*
+* @x: string passed to the function
+* Return: the new string
 */
 
 char *string_toupper(char *x)
@@ -13,7 +12,7 @@ char *string_toupper(char *x)
 
 	for (string = 0; x[string] != '\0';)
 	{
-		if ((x[string] >= 'a') && (x[string] <= 'z'))
+		if (x[string] >= 'a' && x[string] <= 'z')
 		{
 			x[string] = x[string] - 32;
 		}
