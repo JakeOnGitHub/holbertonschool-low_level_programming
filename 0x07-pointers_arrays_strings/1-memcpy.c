3a7dad9c55fd8083750af1b9b1a287bf9171994d
jakeongithub <jakecrandall@gmail.com>
2022-02-08 10:59:58 -0800

    fixed <= to < in for loop commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int x;
 
-	for (x = 0; x <= n; x++)
+	for (x = 0; x < n; x++)
 	{
-		src[x] = dest[x];
+		dest[x] = src[x];
 	}
 	return (dest);
 }
