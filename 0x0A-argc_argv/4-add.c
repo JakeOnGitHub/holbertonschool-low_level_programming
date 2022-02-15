76d7a3f7fc54c3972b9b61d25eb7c0714c5d01a5
jakeongithub <jakecrandall@gmail.com>
2022-02-15 10:46:23 -0800

    add up some argc positions commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* main - entry point
+* @argc: number of arguments
+* @argv: argument strings in an array
+* Return: If an error, 1. Otherwise always 0
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int x;
+	(void) argv;
+
+	if (argc > 2)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			sum += atoi(argv[x]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
