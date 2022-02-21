06232a4381e72ae06af04bbb39e9dbdaaf21fb27
jakeongithub <jakecrandall@gmail.com>
2022-02-21 13:02:17 -0800

    calloc commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,13 +1,27 @@
 #include "main.h"
 
 /**
-*
-*
-*
-*
+* _calloc - entry point.  allocates memory of array
+* @nmemb: number of elements
+* @size: bytes in the element
+* Return: points to the memory
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *ptr;
+	unsigned int x;
 
+	if (nmemb == 0 || size ==0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	for (x = 0; x < nmemb; x++)
+	{
+		ptr[x] = 0;
+	}
+	return (ptr);
 }
