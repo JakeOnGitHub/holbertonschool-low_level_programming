1ac277792cc3ff38e2ce83224de370a7a63976d3
jakeongithub <jakecrandall@gmail.com>
2022-02-22 14:30:34 -0800

    cleaned up / condensed code commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,18 +10,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d == NULL)
-		{
-			d->name = "(nil)";
-		}
-			printf("Name: %s\n", d->name);
-			printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-		{
-			d->owner = "(nil)";
-		}
-			printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
-	else
-		return;
 }
