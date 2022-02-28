2220066ca836386e8d52c95ef4b8174c4af5807e
jakeongithub <jakecrandall@gmail.com>
2022-02-28 11:58:22 -0800

    printf repo commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,8 @@ int main(void)
 int number;
 for (number = 0; number < 10; number++)
 {
-putchar(number % 10 + '0');
-if (number != 9)
+	putchar(number % 10 + '0');
+	if (number != 9)
 {
 putchar(44);
 putchar(' ');
