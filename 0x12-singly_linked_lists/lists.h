697c4166a49248c65f42bfa509cc637d6d985fb2
jakeongithub <jakecrandall@gmail.com>
2022-03-07 12:45:27 -0800

    add node to end of list commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,6 +21,8 @@ typedef struct  list_s
 	struct list_s *next;
 } list_t;
 
+void free_list(list_t *head);
+list_t *add_node_end(list_t **head, const char *str);
 int _strlen(char *s);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
