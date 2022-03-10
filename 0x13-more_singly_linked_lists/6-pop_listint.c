959a216be1bba1c06e5df90a5c0b26497622a923
jakeongithub <jakecrandall@gmail.com>
2022-03-09 19:10:46 -0800

    fixed loop commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,11 +11,11 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (*head == NULL)
-		return (-1);
+	if (head == NULL || *head == NULL)
+		return (0);
 	temp = *head;
-	n = temp->n;
 	*head = temp->next;
+	n = temp->n;
 	free(temp);
 	return (n);
 }
