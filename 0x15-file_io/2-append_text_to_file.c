0829a14b0c2ebce212018d147160d6068ee96b78
jakeongithub <jakecrandall@gmail.com>
2022-03-15 13:34:00 -0700

    append text to a file commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* append_text_to_file - appends text to end of file
+* @filename: file_name
+* @text_content: text
+* Return: Always 0
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+	int len;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	len = strlen(text_content);
+	for (i = 0; i < len; i++)
+	{
+		write(fd, &text_content[i], 1);
+	}
+	return (1);
+}
