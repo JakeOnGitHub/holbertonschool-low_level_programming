0376111b0d16e3cd91d847cfb166b16a4fea8bb7
jakeongithub <jakecrandall@gmail.com>
2022-03-15 08:01:58 -0700

    append text to end of file commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -34,6 +34,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
